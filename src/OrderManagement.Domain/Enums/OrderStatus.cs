0ece112064c6504e8c236204d749fbd17e340e88
jonathancqa <jonathancqaa@gmail.com>
2026-03-13 21:55:54 -0300

    feat: adicionar enum OrderStatus

---
+++ src/OrderManagement.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+namespace OrderManagement.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Draft = 0,
+        Created = 1,
+        Paid = 2,
+        Shipped = 3,        
+        Cancelled = 4
+    }
+}
